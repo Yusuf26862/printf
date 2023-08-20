e238a238812d5f87df2895f4b560791348638bd7
Yusuf <yusuf26862@gmail.com>
2023-08-20 08:22:07 +0000

    printf file

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - print formated string.
+ * @format: the string to be formatted.
+ *
+ * Return: The number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, *format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			*format++;
+			if (*format == '%')
+			{
+				putchar('%');
+			}
+			else if (*format == 'c')
+			{
+				int ch = va_arg(args, int);
+
+				putchar(ch);
+			}
+			else if (*format == 'i' || *format == 'd')
+			{
+				int num = va_arg(args, int);
+
+				putchar(num);
+			}
+		}
+		else if (*format == '\\')
+		{
+			*format++;
+			if (*format == 'n')
+			{
+				putchar('\n');
+			}
+		}
+		else
+		{
+			putchar(*format);
+		}
+		*format++;
+	}
+	return (0);
+}
