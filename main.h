1d5cd7aeba507487d6b124972df4ec07d8b11cdf
Yusuf <yusuf26862@gmail.com>
2023-08-21 19:22:22 +0000

    main.h

--- main.h
+++ main.h
@@ -1,8 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef PRINT_f
+#define PRINT_F
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
+/**
+* struct convert - defines a structure for placeholders and the
+* 		function they call.
+*
+* @sym: The placsholders
+* @f: The function associated
+*/
+
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 
 #endif
