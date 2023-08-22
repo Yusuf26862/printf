f4fca8eb7755bf9c0a659d814c5af10d21c5d779
Yusuf <yusuf26862@gmail.com>
2023-08-22 11:07:29 +0000

    added the call for printing integer

--- format_option.c
+++ format_option.c
@@ -46,3 +46,16 @@ int print_percent(__attribute__((unused))va_list list)
 	_write_char('%');
 	return (1);
 }
+
+/**
+ * print_integer - Prints an integer
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_integer(va_list list)
+{
+	int num_length;
+
+	num_length = print_number(list);
+	return (num_length);
+}
